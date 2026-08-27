a369342e6b642d0bfedc5ef9301e04892cd1ae48
Maryam Doukali <marydouk123@gmail.com>
2026-08-26 19:16:56 -0700

    Created a student introductory panel in Python.

---
+++ about_me.py
@@ -0,0 +1,27 @@
+# Title
+
+print("*" * 40)
+print("\n         Student Profile\n")
+print("*" * 40)
+
+# Student Information (Variables)
+
+first_name = "Maryam"
+last_name = "Doukali"
+age = 21
+city = "Wichita"
+job = "Aerospace Engineering Intern"
+reason_to_learn = '"I want to touch up on my code so that I can get a full-time job as a Software Engineer"'
+fun_fact = "I love performing in operas, musicals, and plays."
+
+#
+
+print(f"Name:       ", first_name, last_name)
+print(f"Age:        ", age)
+print(f"City:       ", city)
+print(f"Current Career: ", job)
+print(f"\nWhy I'm learning to code:\n", reason_to_learn)
+print(f"\nFun fact:", fun_fact)
+print("*" * 40)
+
+
